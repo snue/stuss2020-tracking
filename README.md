3cea708dbc04a86b77ea1e880ca41407260bc4da
Stefan Nuernberger <kabelfrickler@gmail.com>
2020-08-02 21:51:25 +0200

    Add mockup of user data input form
    Just the flask template with a basic input form.
    
    Signed-off-by: Stefan Nuernberger <kabelfrickler@gmail.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+# Corona Visitor Tracking
+
+`startup-services/` - systemd startup scripts for RasPi
+`webui/` - python flask + mysql UI to enter and retrieve data
+`grafana-config/` - dashboard definitions for grafana
+`scanner/` - python script for barcode scanner entry/exit tracking
+
--- webui/app.py
+++ webui/app.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, render_template, request, send_from_directory
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Corona Visitor Tracking"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
