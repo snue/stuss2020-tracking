8f1441d102fc7d948bec59ce8e1f70a386fc60d5
Stefan Nuernberger <kabelfrickler@gmail.com>
2020-08-05 22:27:21 +0200

    database: use datetime and drop stored procedure
    The correct field type for timestamps is datetime.
    Also using python we use mysql.connector and its
    support for prepared statements. So don't put them
    in the database.
    
    Signed-off-by: Stefan Nuernberger <kabelfrickler@gmail.com>

--- setup/create_database.sh
+++ setup/create_database.sh
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS besuchertracker.stammdaten (
 
 CREATE TABLE IF NOT EXISTS besuchertracker.verlaufsdaten (
   id BIGINT NULL AUTO_INCREMENT,
-  zeitstempel DATE,
+  zeitstempel DATETIME,
   besucher_id BIGINT NULL,
   aktion VARCHAR(45) NULL,
   PRIMARY KEY (id));
@@ -26,47 +26,5 @@ CREATE TABLE IF NOT EXISTS besuchertracker.zustandsdaten (
   besucher_id BIGINT NULL,
   zustand VARCHAR(45) NULL,
   PRIMARY KEY (besucher_id));
-
-USE besuchertracker
-
-DELIMITER $$
-CREATE PROCEDURE sp_besucherSpeichern (
-  IN p_besucher_id VARCHAR(45),
-  IN p_name VARCHAR(45),
-  IN p_addresse1 VARCHAR(45),
-  IN p_plz VARCHAR(5),
-  IN p_addresse2 VARCHAR(45),
-  IN p_telefon VARCHAR(20),
-  IN p_email VARCHAR(45),
-  IN p_status VARCHAR(10),
-  IN p_coronawarn BIGINT
-)
-BEGIN
-    insert into stammdaten
-    (
-       besucher_id,
-       name,
-       addresse1,
-       plz,
-       addresse2,
-       telefon,
-       email,
-       status,
-       coronawarn
-    )
-    values
-    (
-       p_besucher_id,
-       p_name,
-       p_addresse1,
-       p_plz,
-       p_addresse2,
-       p_telefon,
-       p_email,
-       p_status,
-       p_coronawarn
-    );
-END$$
-DELIMITER ;
 
 EOF
