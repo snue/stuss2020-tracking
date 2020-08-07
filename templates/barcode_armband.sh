6fd206a122e9915593c48e5e38ab923321768c14
Stefan Nuernberger <kabelfrickler@gmail.com>
2020-08-08 00:29:41 +0200

    barcode: fix off by one error
    This sequence generated one output too much.
    Start at 10001 so the last digits match a user counter.
    
    Signed-off-by: Stefan Nuernberger <kabelfrickler@gmail.com>

--- templates/barcode_armband.sh
+++ templates/barcode_armband.sh
@@ -3,7 +3,7 @@
 # requires barcode
 echo "Wie viele Barcodes sollen generiert werden?"
 read num
-seq 10000 $(( 10000 + num )) | barcode -umm -t 1x10+80+10 -g 20x19+100+4 -o armband.ps
+seq 10001 $(( 10000 + num )) | barcode -umm -t 1x10+80+10 -g 20x19+100+4 -o armband.ps
 
 barcode -b "KOMMT" -b "GEHT" -b "RESERVIERT" -o status.ps -t 1x3 -m 150,60
 
