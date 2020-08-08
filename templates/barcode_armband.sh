6e888fb336aa9da35cef310155d73dc83df445ff
Stefan Nuernberger <kabelfrickler@gmail.com>
2020-08-08 22:15:12 +0200

    barcode: use lowercase status and generate ID examples
    Use lower case input to generate barcodes. The scancodes
    represent keycodes, so uppercase letters are prefixed with
    LSHFT.
    
    Also generate some example IDs in the same script.
    
    Signed-off-by: Stefan Nuernberger <kabelfrickler@gmail.com>

--- templates/barcode_armband.sh
+++ templates/barcode_armband.sh
@@ -5,6 +5,9 @@ echo "Wie viele Barcodes sollen generiert werden?"
 read num
 seq 10001 $(( 10000 + num )) | barcode -umm -t 1x10+80+10 -g 20x19+100+4 -o armband.ps
 
-barcode -b "KOMMT" -b "GEHT" -b "RESERVIERT" -o status.ps -t 1x3 -m 150,60
+
+seq 10001 10010 | barcode -t 2x5 -m 100,50 -o example.ps
+
+barcode -b "kommt" -b "geht" -b "reserviert" -o status.ps -t 1x3 -m 150,60
 
 echo "Fertig"
