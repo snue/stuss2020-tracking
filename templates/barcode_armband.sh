e5d7a9e5c0be0483843b1786114dc8d0fa008dc1
hannes.seidel@ecovery.de <hannes.seidel@ecovery.de>
2020-08-09 19:43:07 +0200

    place and rotate barcode

--- templates/barcode_armband.sh
+++ templates/barcode_armband.sh
@@ -3,7 +3,14 @@
 # requires barcode
 echo "Wie viele Barcodes sollen generiert werden?"
 read num
-seq 10001 $(( 10000 + num )) | barcode -umm -t 1x10+80+10 -g 20x19+100+4 -o armband.ps
+seq 10001 $(( 10000 + num )) | barcode -umm -t 1x20 -g 20x10+85+0 -p A4 -o armband.ps
+# Keine Rotation moeglich ...
+# WORKAROUND #
+ps2pdf armband.ps armbandA3.pdf
+mutool poster -y 2 armbandA3.pdf armbandA4.pdf
+# In Wirklichkeit wird A4 zu A5 gesplittet
+# Das Resultat sind senkrechte Barcodes
+rm armband.ps armbandA3.pdf
 
 barcode -b "KOMMT" -b "GEHT" -b "RESERVIERT" -o status.ps -t 1x3 -m 150,60
 
